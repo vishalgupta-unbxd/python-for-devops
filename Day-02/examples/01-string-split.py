9368e4afbdc169a1736ea5881ea6e8b8ffe6d4f4
Vishal Gupta <vishal.gupta@unbxd.com>
2025-01-27 14:32:35 +0530

    Update split.py

--- Day-02/examples/01-string-split.py
+++ Day-02/examples/01-string-split.py
@@ -1,3 +1,13 @@
 text = "Python is awesome"
 words = text.split()
 print("Words:", words)
+
+
+text = "Python is awesome"
+words = text.split("is")
+print("Words:", words)
+
+
+text = "Python is awesome"
+words = text.split(" ")
+print("Words:", words)
